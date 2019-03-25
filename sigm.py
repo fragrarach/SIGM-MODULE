a4f2d4f56e340f576c06504d3e60fb61eb3f5817
Jan Zalobinski <39808183+fragrarach@users.noreply.github.com>
2019-03-25 09:55:46 -0400

    Added verbose connection print for listen and query cursor. Added SIGM/LOG sub folder handling to add_sql_files().

---
+++ sigm.py
@@ -0,0 +1,71 @@
+import os
+import psycopg2.extensions
+import __main__
+
+
+# Check whether app should reference dev or prod server/db
+def dev_check():
+    raw_filename = os.path.basename(__main__.__file__)
+    removed_extension = raw_filename.split('.')[0]
+    last_word = removed_extension.split('_')[-1]
+    if last_word == 'DEV':
+        return True
+    else:
+        return False
+
+
+# Initialize production DB connection, listen cursor and query cursor
+def sigm_conn(channel=None):
+    global conn_sigm, sigm_query
+    if dev_check():
+        host = '192.168.0.57'
+        dbname = 'DEV'
+    else:
+        host = '192.168.0.250'
+        dbname = 'QuatroAir'
+    conn_sigm = psycopg2.connect(f"host='{host}' dbname='{dbname}' user='SIGM' port='5493'")
+    conn_sigm.set_client_encoding("latin1")
+    conn_sigm.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+
+    if channel:
+        sigm_listen = conn_sigm.cursor()
+        sigm_listen.execute(f"LISTEN {channel};")
+        print(f'Listening to channel {channel} on DB {dbname} at host {host}')
+    sigm_query = conn_sigm.cursor()
+    print(f'SIGM cursor open on DB {dbname} at host {host}')
+
+    return conn_sigm, sigm_query
+
+
+# Initialize log DB connection, listen cursor and query cursor
+def log_conn():
+    global conn_log, log_query
+    if dev_check():
+        host = '192.168.0.57'
+    else:
+        host = '192.168.0.250'
+    conn_log = psycopg2.connect(f"host='{host}' dbname='LOG' user='SIGM' port='5493'")
+    conn_log.set_client_encoding("latin1")
+    conn_log.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+
+    log_query = conn_log.cursor()
+    print(f'Log cursor open on DB LOG at host {host}')
+
+    return conn_log, log_query
+
+
+def add_sql_files():
+    script_path = os.path.realpath(__main__.__file__)
+    parent = os.path.abspath(os.path.join(script_path, os.pardir))
+    sql_folder = parent + '\\SUPPORTING FUNCTIONS'
+    sigm_folder = sql_folder + '\\SIGM'
+    log_folder = sql_folder + '\\LOG'
+    sql_folders = [sigm_folder, log_folder]
+    for folder in sql_folders:
+        if os.path.exists(folder):
+            for file in os.listdir(folder):
+                if file.endswith(".sql"):
+                    file_path = folder + f'\\{file}'
+                    with open(file_path, 'r') as sql_file:
+                        sigm_query.execute(sql_file.read())
+                        print(f'{file} added.')
